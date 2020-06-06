cf7fced15c90fd676d5d93889273c0e441b2b99c
Eric Hale <eric_hale@comcast.net>
2020-06-06 12:13:39 -0600

    Add pad method and test

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,2 +1,8 @@
 class Enigma
+
+  def pad(number)
+    pad_num = number.to_s
+    pad_num.insert(0, "0") until pad_num.size == 5
+    pad_num
+  end
 end
--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -11,4 +11,13 @@ class EnigmaTest < Minitest::Test
   def test_it_exists
     assert_instance_of Enigma, @enigma
   end
+
+  def test_it_can_generate_a_0_padded_number
+    assert_equal "24689", @enigma.pad(24689)
+    assert_equal "04689", @enigma.pad(4689)
+    assert_equal "00689", @enigma.pad(689)
+    assert_equal "00089", @enigma.pad(89)
+    assert_equal "00009", @enigma.pad(9)
+    assert_equal "00000", @enigma.pad(0)
+  end
 end
