69c73d6f613d0d5640036206485b4b6e6cb59a02
Eric Hale <eric_hale@comcast.net>
2020-06-07 16:49:00 -0600

    Add tests and methods for default instance variables

--- lib/shifts.rb
+++ lib/shifts.rb
@@ -1,27 +1,24 @@
 require 'date'
 class Shifts
+  attr_reader :key, :date
 
-  def initialize(key, date)
+  def initialize(key = rand(99999), date = Date.today.strftime("%d%m%y"))
     @key = key
     @date = date
   end
 
-  def zero_pad(key)
+  def zero_pad(key = @key)
     pad_num = key.to_s
     pad_num.insert(0, "0") until pad_num.size == 5
     pad_num
   end
 
-  def shift_keys(key)
+  def shift_keys(key = @key)
     keys = []
     key.chars.each_with_index do |char, index|
       next if index == 4
       keys << char + key[index + 1]
     end
     keys
-  end
-
-  def date
-    date = Date.today.strftime("%d%m%y")
   end
 end
--- test/shifts_test.rb
+++ test/shifts_test.rb
@@ -12,9 +12,24 @@ class ShiftsTest < Minitest::Test
     assert_instance_of Shifts, @shifts
   end
 
+  def test_it_has_attributes
+    assert_equal "02715", @shifts.key
+    assert_equal "040895", @shifts.date
+  end
+
+  def test_it_can_generate_a_random_key_by_default
+    @shifts.stubs(:rand_key).returns(02715)
+    assert_equal 02715 , @shifts.rand_key
+  end
+
+  def test_it_can_get_date_by_default
+    Date.stubs(:today).returns(Date.new(1995, 8, 4))
+    assert_equal "040895", @shifts.date
+  end
+
   def test_it_can_generate_a_0_padded_key
     assert_equal "12715", @shifts.zero_pad(12715)
-    assert_equal "02715", @shifts.zero_pad(2715)
+    assert_equal "02715", @shifts.zero_pad
     assert_equal "00715", @shifts.zero_pad(715)
     assert_equal "00015", @shifts.zero_pad(15)
     assert_equal "00005", @shifts.zero_pad(5)
@@ -23,10 +38,6 @@ class ShiftsTest < Minitest::Test
 
   def test_it_can_split_key_into_keys
     assert_equal ["02", "27", "71", "15"], @shifts.shift_keys("02715")
-  end
-
-  def test_it_can_get_date
-    Date.stubs(:today).returns(Date.new(1995, 8, 4))
-    assert_equal "040895", @shifts.date
+    assert_equal ["02", "27", "71", "15"], @shifts.shift_keys
   end
 end
