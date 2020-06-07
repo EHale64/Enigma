488e78e3a3a96013bdf496983147e3029716598e
Eric Hale <eric_hale@comcast.net>
2020-06-07 16:19:29 -0600

    move zero_pad test and method to Shifts

--- lib/shifts.rb
+++ lib/shifts.rb
@@ -1,2 +1,7 @@
 class Shifts
+  def zero_pad(key)
+    pad_num = key.to_s
+    pad_num.insert(0, "0") until pad_num.size == 5
+    pad_num
+  end
 end
--- test/shifts_test.rb
+++ test/shifts_test.rb
@@ -11,4 +11,13 @@ class ShiftsTest < Minitest::Test
   def test_it_exists
     assert_instance_of Shifts, @shifts
   end
+
+  def test_it_can_generate_a_0_padded_key
+    assert_equal "12715", @shifts.zero_pad(12715)
+    assert_equal "02715", @shifts.zero_pad(2715)
+    assert_equal "00715", @shifts.zero_pad(715)
+    assert_equal "00015", @shifts.zero_pad(15)
+    assert_equal "00005", @shifts.zero_pad(5)
+    assert_equal "00000", @shifts.zero_pad(0)
+  end
 end
